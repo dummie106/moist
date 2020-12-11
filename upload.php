0d89fb4b28ba4b9924f39dbde7dc1977b31255ce
dummie106 <71605111+dummie106@users.noreply.github.com>
2020-12-11 15:01:15 -0500

    Update upload.php

--- upload.php
+++ upload.php
@@ -47,4 +47,19 @@ if ($uploadOk == 0) {
   }
 }
 ?>
+<html>
+  <body>
+    <!DOCTYPE html>
+<html>
+<body>
 
+<form action="" method="post" enctype="multipart/form-data">
+  Select image to upload:
+  <input type="file" name="fileToUpload" id="fileToUpload">
+  <input type="submit" value="Upload Image" name="submit">
+</form>
+
+</body>
+</html>
+  </body>
+</html>
